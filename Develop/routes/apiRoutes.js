3bb9a3d2a3d3beaf9ae49464c755d767c99413e7
PAVLO DEMENSHYN <89635409+Paul-Dimenshion@users.noreply.github.com>
2023-01-23 00:13:48 -0400

    routes

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,40 @@
+// dependencies
+import express from "express";
+const router = express.Router();
+
+// brings in the DB class object
+import DB from '../db/DB';
+
+// route to get notes
+router.get("/api/notes", async function (req, res) {
+  const notes = await DB.readNotes();
+  return res.json(notes);
+});
+
+// route to add a new note and add it to the json file
+router.post("/api/notes", async function (req, res) {
+  const currentNotes = await DB.readNotes();
+  let newNote = {
+    title: req.body.title,
+    text: req.body.text,
+  };
+
+  await DB.addNote([...currentNotes, newNote]);
+
+  return res.send(newNote);
+});
+
+// // route to delete notes
+router.delete("/api/notes/:id", async function (req, res) {
+  // separates out the note to delete based on id
+  const noteToDelete = req.params.id;
+  // notes already in json file
+  const currentNotes = await DB.readNotes();
+  // sort through notes file and create a new array minus the note in question
+  const newNoteData = currentNotes.filter((note) => note.id !== noteToDelete);
+
+  // sends the new array back the DB class 
+  await DB.deleteNote(newNoteData);
+  
+  return res.send(newNoteData);
+});
